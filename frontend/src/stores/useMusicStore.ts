d89443b8f5c8cfc19040ae7afec338add93921a8
Enrique Shanna <enriqueshanna00@gmail.com>
2025-03-30 02:12:26 +0800

    created Home Page

--- frontend/src/stores/useMusicStore.ts
+++ frontend/src/stores/useMusicStore.ts
@@ -10,9 +10,16 @@ interface MusicStore {
    isLoading: boolean;
    error: string | null;
    currentAlbum: Album | null;
+   madeForYouSongs:Song[];
+   trendingSongs:Song[];
+   featuredSongs:Song[];
    
    fetchAlbums: () => Promise<void>;
    fetchAlbumById: (id: string) => Promise<void>;
+   fetchFeaturedSongs: () => Promise<void>;
+   fetchMadeForYouSongs: () => Promise<void>;
+   fetchTrendingSongs: () => Promise<void>;
+
 }
 
 
@@ -22,6 +29,9 @@ export const useMusicStore = create<MusicStore>((set) => ({
    isLoading: false,
    error: null,
    currentAlbum: null,
+   madeForYouSongs: [],
+   featuredSongs: [],
+   trendingSongs: [],
 
    fetchAlbums: async () => {
     //data fetch logic...
@@ -50,4 +60,39 @@ export const useMusicStore = create<MusicStore>((set) => ({
         set({isLoading: false});
     }
    },
+
+   fetchFeaturedSongs: async () => {set({ isLoading: true, error: null });
+   try {
+       const response = await axiosInstance.get("/songs/featured");
+       set({ featuredSongs: response.data });
+   } catch (error: any) {
+       set({ error: error.response.data.message });
+   } finally {
+       set({ isLoading: false });
+   }},
+
+
+   fetchMadeForYouSongs: async () => {
+    set({ isLoading: true, error: null });
+		try {
+			const response = await axiosInstance.get("/songs/made-for-you");
+			set({ madeForYouSongs: response.data });
+		} catch (error: any) {
+			set({ error: error.response.data.message });
+		} finally {
+			set({ isLoading: false });
+		}
+   },
+   fetchTrendingSongs: async () => {
+    set({ isLoading: true, error: null });
+		try {
+			const response = await axiosInstance.get("/songs/trending");
+			set({ trendingSongs: response.data });
+		} catch (error: any) {
+			set({ error: error.response.data.message });
+		} finally {
+			set({ isLoading: false });
+		}
+   },
+
 }));
