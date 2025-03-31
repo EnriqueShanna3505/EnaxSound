fd83b80765d43ee901daa0805d467dc419c55980
Enrique Shanna <enriqueshanna00@gmail.com>
2025-03-31 10:01:09 +0800

    create delete song function, and fixed admin routes.

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -9,14 +9,15 @@ import {
 import { protectRoute, requireAdmin } from '../middleware/auth.middleware.js';
 
 const router = Router();
-//slightly optimized
+
 router.use(protectRoute, requireAdmin);
 
 router.get('/check', checkAdmin);
 
 router.post('/songs', createSong);
-router.delete('/songs:/:id', deleteSong);
+router.delete('/songs/:id', deleteSong);
 
 router.post('/albums', createAlbum);
 router.delete('/albums/:id', deleteAlbum);
+
 export default router;
